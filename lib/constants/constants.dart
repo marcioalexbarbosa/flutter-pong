7c9f7594da37b630c79f2764064bbc643c1b2a43
Marcio Alexandre Barbosa <m.alexan@hotmail.com>
2021-06-09 16:59:50 -0300

    Delete lib directory

--- lib/constants/constants.dart
+++
@@ -1,7 +0,0 @@
-const posIncrement = 5.0;
-const animationControllerDuration = 10000;
-const tweenEnd = 100.0;
-const batWidthFactor = 7;
-const batHeightFactor = 30;
-const diameterBall = 30;
-const speedAi = 600;
